5029200365f088b110289d5ae74cda84c3392d83
marceloarmando <mcuellarr98@gmail.com>
2021-03-02 21:30:10 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require ('express')
+const stripe = require ('stripe')
+('sk_test_51IPdRlGVQdBXbXkJjqTVNffab2ve4Vpr9bF7CExsAVsQMW4ut1FjfjqXFbIgiPZHDFsItquYt7CF8aO9IMqAnZTH00pUtaXg3K');
+
+const cors=require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/stripe_checkout', async (req,res)=>{
+    const stripeToken =req.body.stripeToken;
+    const cantidad = req.body.cantidad;
+
+    const cantidadInPen = Math.round(cantidad*100);
+    const chargeObject=await stripe.charges.create({
+
+        amount: cantidadInPen,
+        currency:'pen',
+        source:stripeToken,
+        capture: false,
+        description: 'Pago Stripe',
+        receipt_email:'mcuellarr@gmail.com'
+    });
+
+    try {
+        await stripe.charges.capture(chargeObject.id);
+        res.json(chargeObject);
+
+
+    }catch(error){
+
+        await stripe.refunds.create({charge: chargeObject.id});
+        res.json(chargeObject);
+
+    };
+    
+});
+app.listen(3000,()=>{
+    console.log('Server en PORT 3000')
+})
